47074ca5b93765afe6f45ec58b242a66a2a8b10b
Josef Walek <63097858+robobo789@users.noreply.github.com>
2020-11-09 00:15:18 +0100

    1.0
    RPM control
    STEP control
    
    no emergency...

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,8 @@
 #define encR_pinB 4 //outoutB digital pin3
 
 void isrA(), isrB();
-#define read_encL_A bitRead(PIND, encL_pinA)         //faster than digitalRead()
-#define read_encL_B bitRead(PIND, encL_pinB)         //faster than digitalRead()
+#define read_encL_A bitRead(PIND, encL_pinA) //faster than digitalRead()
+#define read_encL_B bitRead(PIND, encL_pinB) //faster than digitalRead()
 #define read_encR_A bitRead(PIND, encR_pinA) //faster than digitalRead()
 #define read_encR_B bitRead(PIND, encR_pinB) //faster than digitalRead()
 
@@ -39,21 +39,24 @@ long prot_step_timeR;
 int emergency_stop = -10;
 int emergency_stop_cooldown = 0;
 
-double stepL_time, desired_stepL_time, stepL_time_PID_output;
-double step_timeLKp = 0.0001, step_timeLKi = 0, step_timeLKd = 0;
-PID stepL_timePID(&stepL_time, &stepL_time_PID_output, &desired_stepL_time, step_timeLKp, step_timeLKi, step_timeLKd, DIRECT);
+double RPM_L, desired_RPM_L, RPM_L_PID_output;
+double RPMLKp = 5, RPMLKi = 1, RPMLKd = 0;
+PID RPM_L_PID(&RPM_L, &RPM_L_PID_output, &desired_RPM_L, RPMLKp, RPMLKi, RPMLKd, DIRECT);
 
-double stepR_time, desired_stepR_time, stepR_time_PID_output;
-double step_timeRKp = 0.0001, step_timeRKi = 0, step_timeRKd = 0;
-PID stepR_timePID(&stepR_time, &stepR_time_PID_output, &desired_stepR_time, step_timeRKp, step_timeRKi, step_timeRKd, DIRECT);
+double RPM_R, desired_RPM_R, RPM_R_PID_output;
+double RPMRKp = 5, RPMRKi = 1, RPMRKd = 0;
+PID RPM_R_PID(&RPM_R, &RPM_R_PID_output, &desired_RPM_R, RPMRKp, RPMRKi, RPMRKd, DIRECT);
 
 double stepsL, desired_stepsL, stepL_PID_output;
-double stepLKp = 5, stepLKi = 0, stepLKd = 0;
+double stepLKp = 1, stepLKi = 0, stepLKd = 0;
 PID stepL_PID(&stepsL, &stepL_PID_output, &desired_stepsL, stepLKp, stepLKi, stepLKd, DIRECT);
 
 double stepsR, desired_stepsR, stepR_PID_output;
-double stepRKp = 5, stepRKi = 0, stepRKd = 0;
+double stepRKp = 1, stepRKi = 0, stepRKd = 0;
 PID stepR_PID(&stepsR, &stepR_PID_output, &desired_stepsR, stepRKp, stepRKi, stepRKd, DIRECT);
+
+int motorL_pwm;
+int motorR_pwm;
 
 void setup()
 {
@@ -73,27 +76,30 @@ void setup()
   attachInterrupt(digitalPinToInterrupt(encL_pinA), isrA, RISING);
   attachInterrupt(digitalPinToInterrupt(encR_pinA), isrB, RISING);
 
-  desired_stepL_time = 0;
-  stepL_time = 0;
-  stepL_timePID.SetOutputLimits(-255, 255);
-  stepL_timePID.SetSampleTime(50);
-  stepL_timePID.SetMode(AUTOMATIC);
+  motorL_pwm = 0;
+  motorR_pwm = 0;
 
-  desired_stepR_time = 0;
-  stepR_time = 0;
-  stepR_timePID.SetOutputLimits(-255, 255);
-  stepR_timePID.SetSampleTime(50);
-  stepR_timePID.SetMode(AUTOMATIC);
+  desired_RPM_L = 0;
+  RPM_L = 0;
+  RPM_L_PID.SetOutputLimits(-255, 255);
+  RPM_L_PID.SetSampleTime(50);
+  RPM_L_PID.SetMode(AUTOMATIC);
+
+  desired_RPM_R = 0;
+  RPM_R = 0;
+  RPM_R_PID.SetOutputLimits(-255, 255);
+  RPM_R_PID.SetSampleTime(50);
+  RPM_R_PID.SetMode(AUTOMATIC);
 
   desired_stepsL = 200;
   stepsL = 0;
-  stepL_PID.SetOutputLimits(-200, 200);
+  stepL_PID.SetOutputLimits(-150, 150);
   stepL_PID.SetSampleTime(50);
   stepL_PID.SetMode(AUTOMATIC);
 
   desired_stepsR = 200;
   stepsR = 0;
-  stepR_PID.SetOutputLimits(-200, 200);
+  stepR_PID.SetOutputLimits(-150, 150);
   stepR_PID.SetSampleTime(50);
   stepR_PID.SetMode(AUTOMATIC);
 }
@@ -101,15 +107,17 @@ void setup()
 void motorL(int velocity)
 {
   velocity = constrain(velocity, -255, 255);
-  if (velocity > 20)
+  if (velocity > 10)
   {
+    velocity = map(velocity, 0, 255, 50, 255);
     analogWrite(motorA, velocity);
     digitalWrite(motorA1, HIGH);
     digitalWrite(motorA2, LOW);
   }
-  else if (velocity < 20)
+  else if (velocity < -10)
   {
-    analogWrite(motorA, -velocity);
+    velocity = map(velocity, 0, -255, 50, 255);
+    analogWrite(motorA, velocity);
     digitalWrite(motorA1, LOW);
     digitalWrite(motorA2, HIGH);
   }
@@ -125,15 +133,17 @@ void motorR(int velocity)
 {
   velocity = constrain(velocity, -255, 255);
 
-  if (velocity > 20)
+  if (velocity > 10)
   {
+    velocity = map(velocity, 0, 255, 50, 255);
     analogWrite(motorB, velocity);
     digitalWrite(motorB1, HIGH);
     digitalWrite(motorB2, LOW);
   }
-  else if (velocity < 20)
+  else if (velocity < -10)
   {
-    analogWrite(motorB, -velocity);
+    velocity = map(velocity, 0, -255, 50, 255);
+    analogWrite(motorB, velocity);
     digitalWrite(motorB1, LOW);
     digitalWrite(motorB2, HIGH);
   }
@@ -145,20 +155,18 @@ void motorR(int velocity)
   }
 }
 
-
-
 //INTERRUPT SERVICE ROUTINES
 void isrA()
 {
   if (read_encL_B == HIGH)
   {
-    step_countL--;
+    step_countL++;
     step_timeL = (micros() - prev_timeL);
     prev_timeL = micros();
   }
   else
   {
-    step_countL++;
+    step_countL--;
     step_timeL = -(micros() - prev_timeL);
     prev_timeL = micros();
   }
@@ -168,13 +176,13 @@ void isrB()
 {
   if (read_encR_B == HIGH)
   {
-    step_countR++;
+    step_countR--;
     step_timeR = -(micros() - prev_timeR);
     prev_timeR = micros();
   }
   else
   {
-    step_countR--;
+    step_countR++;
     step_timeR = (micros() - prev_timeR);
     prev_timeR = micros();
   }
@@ -183,105 +191,92 @@ void isrB()
 void load_ISR_values()
 {
   noInterrupts();
-  prot_step_countL = constrain(step_countL, -200000, 200000);
-  prot_step_countR = constrain(step_countR, -200000, 200000);
-  prot_step_timeL = step_timeL;
-  prot_step_timeR = step_timeR;
+
+  if ((micros() - prev_timeL) >= 100000)
+  {
+    step_timeL = 100000;
+    prev_timeL = micros();
+  }
+
+  if ((micros() - prev_timeR) >= 100000)
+  {
+    step_timeR = 100000;
+    prev_timeR = micros();
+  }
+
+  prot_step_countL = step_countL;
+  prot_step_countR = step_countR;
+  prot_step_timeL = constrain(step_timeL, -100000, 100000);
+  prot_step_timeR = constrain(step_timeR, -100000, 100000);
   interrupts();
+
+
+  if (prot_step_timeL != 100000)
+  {
+    RPM_L = 60000000 / (75 * prot_step_timeL);
+  }
+  else
+  {
+    RPM_L = 0;
+  }
+
+  if (prot_step_timeR != 100000)
+  {
+    RPM_R = 60000000 / (75 * prot_step_timeR);
+  }
+  else
+  {
+    RPM_R = 0;
+  }
 }
 
+void SendSerial()
+{
+  Serial.print(desired_RPM_L - RPM_L); //STEP LEFT TIME ERROR
+  Serial.print(",");
+  Serial.print(desired_RPM_R - RPM_R); //STEP RIGT TIME ERROR
+  Serial.print(",");
+  Serial.print(desired_stepsL - stepsL); //STEP LEFT COUNT ERROR
+  Serial.print(",");
+  Serial.print(desired_stepsR - stepsR); //STEP RIGHT COUNT ERROR
+  Serial.print(",");
+  Serial.print(stepL_PID_output); //
+  Serial.print(",");
+  Serial.print(stepR_PID_output); //
+  Serial.print(",");
+  Serial.print(RPM_L_PID_output); //
+  Serial.print(",");
+  Serial.println(RPM_R_PID_output); //
+  // Serial.print(",");
+  // Serial.print(prot_step_timeL);
+  // Serial.print(",");
+  // Serial.print(prot_step_timeR);
+  // Serial.print(",");
+  // Serial.print(RPM_L);
+  // Serial.print(",");
+  // Serial.println(RPM_R);
 
-
-
-void SendSerial(){
-    Serial.print(desired_stepL_time - prot_step_timeL);  //STEP LEFT TIME ERROR 
-    Serial.print(",");
-    Serial.print(desired_stepR_time - prot_step_timeR);  //STEP RIGT TIME ERROR 
-    Serial.print(",");
-    Serial.print(desired_stepsL - stepsL);              //STEP LEFT COUNT ERROR
-    Serial.print(",");
-    Serial.print(desired_stepsR - stepsR);              //STEP RIGHT COUNT ERROR
-    Serial.print(",");
-    Serial.print(stepL_PID_output);                     //
-    Serial.print(",");
-    Serial.print(stepR_PID_output);                     //
-    Serial.print(",");    
-    Serial.print(stepL_time_PID_output);          //
-    Serial.print(",");  
-    Serial.print(stepR_time_PID_output);          //
-    Serial.print(",");
-    Serial.print(prot_step_timeL); 
-    Serial.print(",");
-    Serial.println(prot_step_timeR);                           //                        //
-
-    prev_step_countL = prot_step_countL;
-    prev_step_countR = prot_step_countR;
+  prev_step_countL = prot_step_countL;
+  prev_step_countR = prot_step_countR;
 }
-
 
 void loop()
 {
   load_ISR_values();
+  stepsL = prot_step_countL;
+  stepsR = prot_step_countR;
 
-  // stepsL = prot_step_countL;
-  // stepL_PID.Compute();
-  // stepsR = prot_step_countR;
-  // stepR_PID.Compute();
+  stepL_PID.Compute();
+  stepR_PID.Compute();
 
-  // if (stepsL > desired_stepsL){
-  //   stepL_PID_output = 200 + stepL_PID_output;
-  // } else if (stepsL < desired_stepsL)
-  // {
-  //   stepL_PID_output = 200 - stepL_PID_output;
-  // } else 
-  // {
-  //   stepL_PID_output = 200;
-  // }
+  desired_RPM_L = stepL_PID_output;
+  desired_RPM_R = stepR_PID_output;
 
-  // if (stepsR > desired_stepsR){
-  //   stepR_PID_output = 200 + stepR_PID_output;
-  // } else if (stepsR < desired_stepsR)
-  // {
-  //   stepR_PID_output = 200 - stepR_PID_output;
-  // } else 
-  // {
-  //   stepR_PID_output = 200;
-  // }
+  RPM_L_PID.Compute();
+  RPM_R_PID.Compute();
 
-
-  // desired_stepL_time = stepL_PID_output * 1000;
-  // desired_stepR_time = stepR_PID_output * 1000;
-
-  desired_stepL_time = 100000;
-  desired_stepR_time = 100000;
-
-  stepL_time = prot_step_timeL;
-  stepL_timePID.Compute();
-  stepR_time = prot_step_timeR;
-  stepR_timePID.Compute();
-
-// if (step_timeL > desired_stepL_time){
-//     stepL_time_PID_output = 255 - stepL_time_PID_output;
-//   } else if (stepL_time < desired_stepL_time)
-//   {
-//     stepL_time_PID_output = 255 + stepL_time_PID_output;
-//   } else 
-//   {
-//     stepL_time_PID_output = 0;
-//   }
-
-//   if (stepR_time > desired_stepR_time){
-//     stepR_time_PID_output = 255 + stepR_time_PID_output;
-//   } else if (stepR_time < desired_stepR_time)
-//   {
-//     stepR_time_PID_output = 255 - stepR_time_PID_output;
-//   } else 
-//   {
-//     stepR_time_PID_output = 0;
-//   }
-
-  motorL(stepL_time_PID_output);
-  motorR(stepR_time_PID_output);
+  motorL(RPM_L_PID_output);
+  motorR(RPM_R_PID_output);
 
   SendSerial();
 }
