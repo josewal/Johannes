74e1801df1f38434d9cccd2c5707a1404e5f3321
Josef Walek <63097858+robobo789@users.noreply.github.com>
2020-11-08 22:43:34 +0100

    0.3
    Added just serial sender
    
    Trying to fix the stepTimePID

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,14 @@
 #define motorB1 10
 #define motorB2 9
 
-#define encL_pinA 2 //outputA digital pin2
-#define encL_pinB 4 //outoutB digital pin3
-#define encR_pinA 3 //outputA digital pin2
-#define encR_pinB 5 //outoutB digital pin3
+#define encL_pinA 3 //outputA digital pin2
+#define encL_pinB 5 //outoutB digital pin3
+#define encR_pinA 2 //outputA digital pin2
+#define encR_pinB 4 //outoutB digital pin3
 
 void isrA(), isrB();
-#define read_encL_A bitRead(PIND, 2)         //faster than digitalRead()
-#define read_encL_B bitRead(PIND, 4)         //faster than digitalRead()
+#define read_encL_A bitRead(PIND, encL_pinA)         //faster than digitalRead()
+#define read_encL_B bitRead(PIND, encL_pinB)         //faster than digitalRead()
 #define read_encR_A bitRead(PIND, encR_pinA) //faster than digitalRead()
 #define read_encR_B bitRead(PIND, encR_pinB) //faster than digitalRead()
 
@@ -40,11 +40,11 @@ int emergency_stop = -10;
 int emergency_stop_cooldown = 0;
 
 double stepL_time, desired_stepL_time, stepL_time_PID_output;
-double step_timeLKp = 0.001, step_timeLKi = 0.01, step_timeLKd = 0;
+double step_timeLKp = 0.0001, step_timeLKi = 0, step_timeLKd = 0;
 PID stepL_timePID(&stepL_time, &stepL_time_PID_output, &desired_stepL_time, step_timeLKp, step_timeLKi, step_timeLKd, DIRECT);
 
 double stepR_time, desired_stepR_time, stepR_time_PID_output;
-double step_timeRKp = 0.001, step_timeRKi = 0.01, step_timeRKd = 0;
+double step_timeRKp = 0.0001, step_timeRKi = 0, step_timeRKd = 0;
 PID stepR_timePID(&stepR_time, &stepR_time_PID_output, &desired_stepR_time, step_timeRKp, step_timeRKi, step_timeRKd, DIRECT);
 
 double stepsL, desired_stepsL, stepL_PID_output;
@@ -101,13 +101,13 @@ void setup()
 void motorL(int velocity)
 {
   velocity = constrain(velocity, -255, 255);
-  if (velocity > 0)
+  if (velocity > 20)
   {
     analogWrite(motorA, velocity);
     digitalWrite(motorA1, HIGH);
     digitalWrite(motorA2, LOW);
   }
-  else if (velocity < 0)
+  else if (velocity < 20)
   {
     analogWrite(motorA, -velocity);
     digitalWrite(motorA1, LOW);
@@ -125,13 +125,13 @@ void motorR(int velocity)
 {
   velocity = constrain(velocity, -255, 255);
 
-  if (velocity > 0)
+  if (velocity > 20)
   {
     analogWrite(motorB, velocity);
     digitalWrite(motorB1, HIGH);
     digitalWrite(motorB2, LOW);
   }
-  else if (velocity < 0)
+  else if (velocity < 20)
   {
     analogWrite(motorB, -velocity);
     digitalWrite(motorB1, LOW);
@@ -144,6 +144,8 @@ void motorR(int velocity)
     digitalWrite(motorB2, LOW);
   }
 }
+
+
 
 //INTERRUPT SERVICE ROUTINES
 void isrA()
@@ -173,7 +175,7 @@ void isrB()
   else
   {
     step_countR--;
-    step_timeR = micros() - prev_timeR;
+    step_timeR = (micros() - prev_timeR);
     prev_timeR = micros();
   }
 }
@@ -181,45 +183,105 @@ void isrB()
 void load_ISR_values()
 {
   noInterrupts();
-  prot_step_countL = step_countL;
-  prot_step_countR = step_countR;
+  prot_step_countL = constrain(step_countL, -200000, 200000);
+  prot_step_countR = constrain(step_countR, -200000, 200000);
   prot_step_timeL = step_timeL;
   prot_step_timeR = step_timeR;
   interrupts();
 }
 
+
+
+
+void SendSerial(){
+    Serial.print(desired_stepL_time - prot_step_timeL);  //STEP LEFT TIME ERROR 
+    Serial.print(",");
+    Serial.print(desired_stepR_time - prot_step_timeR);  //STEP RIGT TIME ERROR 
+    Serial.print(",");
+    Serial.print(desired_stepsL - stepsL);              //STEP LEFT COUNT ERROR
+    Serial.print(",");
+    Serial.print(desired_stepsR - stepsR);              //STEP RIGHT COUNT ERROR
+    Serial.print(",");
+    Serial.print(stepL_PID_output);                     //
+    Serial.print(",");
+    Serial.print(stepR_PID_output);                     //
+    Serial.print(",");    
+    Serial.print(stepL_time_PID_output);          //
+    Serial.print(",");  
+    Serial.print(stepR_time_PID_output);          //
+    Serial.print(",");
+    Serial.print(prot_step_timeL); 
+    Serial.print(",");
+    Serial.println(prot_step_timeR);                           //                        //
+
+    prev_step_countL = prot_step_countL;
+    prev_step_countR = prot_step_countR;
+}
+
+
 void loop()
 {
   load_ISR_values();
 
-  stepsL = prot_step_countL;
-  stepL_PID.Compute();
-  stepsR = prot_step_countR;
-  stepR_PID.Compute();
+  // stepsL = prot_step_countL;
+  // stepL_PID.Compute();
+  // stepsR = prot_step_countR;
+  // stepR_PID.Compute();
 
-  desired_stepL_time = stepL_PID_output * 1000;
-  desired_stepR_time = stepR_PID_output * 1000;
+  // if (stepsL > desired_stepsL){
+  //   stepL_PID_output = 200 + stepL_PID_output;
+  // } else if (stepsL < desired_stepsL)
+  // {
+  //   stepL_PID_output = 200 - stepL_PID_output;
+  // } else 
+  // {
+  //   stepL_PID_output = 200;
+  // }
+
+  // if (stepsR > desired_stepsR){
+  //   stepR_PID_output = 200 + stepR_PID_output;
+  // } else if (stepsR < desired_stepsR)
+  // {
+  //   stepR_PID_output = 200 - stepR_PID_output;
+  // } else 
+  // {
+  //   stepR_PID_output = 200;
+  // }
+
+
+  // desired_stepL_time = stepL_PID_output * 1000;
+  // desired_stepR_time = stepR_PID_output * 1000;
+
+  desired_stepL_time = 100000;
+  desired_stepR_time = 100000;
 
   stepL_time = prot_step_timeL;
   stepL_timePID.Compute();
   stepR_time = prot_step_timeR;
   stepR_timePID.Compute();
 
+// if (step_timeL > desired_stepL_time){
+//     stepL_time_PID_output = 255 - stepL_time_PID_output;
+//   } else if (stepL_time < desired_stepL_time)
+//   {
+//     stepL_time_PID_output = 255 + stepL_time_PID_output;
+//   } else 
+//   {
+//     stepL_time_PID_output = 0;
+//   }
+
+//   if (stepR_time > desired_stepR_time){
+//     stepR_time_PID_output = 255 + stepR_time_PID_output;
+//   } else if (stepR_time < desired_stepR_time)
+//   {
+//     stepR_time_PID_output = 255 - stepR_time_PID_output;
+//   } else 
+//   {
+//     stepR_time_PID_output = 0;
+//   }
+
   motorL(stepL_time_PID_output);
-  motorR(stepL_time_PID_output);
+  motorR(stepR_time_PID_output);
 
-  if ((prev_step_countL != prot_step_countL) or (prev_step_countR != prot_step_countR))
-  {
-    Serial.print("stepL_time \t");
-    Serial.print(stepL_time);
-    Serial.print("\t stepL_time FACTOR R\t");
-    Serial.print(stepL_time_PID_output);
-    Serial.print("\t RPM L \t");
-    Serial.print(prot_step_timeL);
-    Serial.print("\t RPM R \t");
-    Serial.println(prot_step_timeR);
-
-    prev_step_countL = prot_step_countL;
-    prev_step_countR = prot_step_countR;
-  }
+  SendSerial();
 }
